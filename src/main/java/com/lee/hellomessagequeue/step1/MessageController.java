395be4990b93967babe1ec12d5f9486e474b97ed
YONGHO LEE <mylovedarts@naver.com>
2025-01-28 20:27:08 +0900

    step0 -> step1 수징

--- src/main/java/com/lee/hellomessagequeue/step1/MessageController.java
+++ src/main/java/com/lee/hellomessagequeue/step1/MessageController.java
@@ -1,4 +1,4 @@
-package com.lee.hellomessagequeue.step0;
+package com.lee.hellomessagequeue.step1;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/lee/hellomessagequeue/step1/RabbitMQConfig.java
+++ src/main/java/com/lee/hellomessagequeue/step1/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package com.lee.hellomessagequeue.step0;
+package com.lee.hellomessagequeue.step1;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
