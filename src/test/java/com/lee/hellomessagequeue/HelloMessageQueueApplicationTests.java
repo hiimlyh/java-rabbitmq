34f23f2426c24e4887eba2daf9140eb6c5c289bc
YONGHO LEE <mylovedarts@naver.com>
2025-01-28 18:52:31 +0900

    init

---
+++ src/test/java/com/lee/hellomessagequeue/HelloMessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.hellomessagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessageQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
